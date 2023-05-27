718279ec4d52e70cc21dc67c21b207ac0bc36646
Sebastian <sebastiangpeirano@gmail.com>
2023-05-27 16:51:32 -0300

    trabajando en el avatar

--- sistema_coder/urls.py
+++ sistema_coder/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from sistema_coder.views import *
 from contol_estudios.views import lista_estudiantes, lista_cursos
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +31,4 @@ urlpatterns = [
     path("perfiles/",include('perfiles.urls')),
     path("",inicio, name="Inicio"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
